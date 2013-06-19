a872102172fa679f0824edf06169895ac62d99a9
Josh Argent <josh@vanillaworld.org>
2013-06-19 16:49:35 +0100

    Implemented the /vault command so people can view their first vault.

--- Vaults/src/org/vanillaworld/Vaults/Main.java
+++ Vaults/src/org/vanillaworld/Vaults/Main.java
@@ -1,9 +1,13 @@
 package org.vanillaworld.Vaults;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.vanillaworld.Vaults.Exceptions.NotEnoughVaultsException;
+import org.vanillaworld.Vaults.Exceptions.VaultNotFoundException;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -27,7 +31,13 @@ public class Main extends JavaPlugin implements Listener {
 		{
 			if(args.length == 0)
 			{
-				
+				try {
+					Functions.openVault((Player) sender, sender.getName(), 1);
+				} catch (VaultNotFoundException e) {
+					sender.sendMessage(ChatColor.RED + "You're vault is empty!");
+				} catch (NotEnoughVaultsException e) {
+					sender.sendMessage(ChatColor.RED + "You are not allowed to have " + e.vaultID + " vaults!");
+				}
 			}
 			else if(args.length == 1)
 			{
